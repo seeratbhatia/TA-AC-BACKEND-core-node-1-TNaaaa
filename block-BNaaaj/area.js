aeeae7462641c2603907fd4a8f52a13a8212cdf8
seeratbhatia <b.seerat@gmail.com>
2022-03-22 17:58:50 +0530

    assignment

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,21 @@
+function square(a) {
+    return a * a;
+
+}
+
+function rectangle(a, b) {
+    return a * b;
+
+}
+
+function circle(r) {
+    return 2 * 3.14 * r;
+
+}
+
+module.exports = {
+    square: square,
+    rectangle: rectangle,
+    circle: circle
+}
+
