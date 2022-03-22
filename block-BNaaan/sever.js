4667899c08d27ccd17463ee41ecc003c8e990a54
seeratbhatia <b.seerat@gmail.com>
2022-03-22 22:21:23 +0530

    block-2

--- block-BNaaan/sever.js
+++ block-BNaaan/sever.js
@@ -1,1 +1,10 @@
-var = 
+var http = require('http');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    console.log(req.headers);
+    console.log(req.method, req.url);
+}
+
+server.listen(3000);
