2a8570acabf5dd11a01ae054c78aef6d0c9f2921
seeratbhatia <b.seerat@gmail.com>
2022-03-24 16:13:05 +0530

    block-4

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -1,1 +1,15 @@
 var http = require('http');
+
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    console.log(req.method);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h2>Welcome to Altcampus</h2>')
+}
+
+server.listen(4444, () => {
+    console.log('server listening on port 4444');
+})
