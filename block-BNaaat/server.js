e7e8d588d82bce3680863c1c00d27b74476fce8c
seeratbhatia <b.seerat@gmail.com>
2022-03-24 22:34:46 +0530

    block-2

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -6,12 +6,6 @@ function handleRequest(req, res) {
     if(req.method == 'GET' && req.url == '/') {
         res.setHeader('Content-type', 'text/plain');
         res.end('Welcome to homepage');
-    } else if(req.method == 'GET' && req.url == '/about') {
-        res.setHeader('Conent-type', 'text/html');
-        res.end('<h2>this is all about NodeJS</h2>')
-    } else if(req.method == 'POST' && req.url == '/about') {
-        res.setHeader('Conent-type', 'application/json');
-        res.end(`{message: this is a post request}`);
     }
 }
 
